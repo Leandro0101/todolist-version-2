251bceeae9b4b66a45e47aa0ee43c694df5c8057
Leandro0101 <leandrovieira3306@gmail.com>
2020-12-20 14:46:10 -0300

    part 2 of panel's start

--- src/components/panel/panel_navegation/start/form/form.js
+++ src/components/panel/panel_navegation/start/form/form.js
@@ -3,17 +3,20 @@ import './form.css'
 const Form = () => {
   return (
     <form>
-      <div class="form-item">
-        <label class="form-label">Título</label>
-        <input type="text" name='title' class="form-control" placeholder="name@example.com" />
+      <div className="form-item">
+        <label className="form-label">Título</label>
+        <input type="text" name='title' className="form-control" placeholder="name@example.com" />
       </div>
-      <div class="form-item">
-        <label class="form-label">Descrição</label>
-        <textarea class="form-control"></textarea>
+      <div className="form-item">
+        <label className="form-label">Descrição</label>
+        <textarea className="form-control"></textarea>
       </div>
-      <div class="form-item">
-        <label class="form-label">Concluir até: </label>
-        <input type="date" class="form-control" />
+      <div className="form-item">
+        <label className="form-label">Concluir até: </label>
+        <input type="date" className="form-control" />
+      </div>
+      <div className="form-item">
+        <button>Adicionar tarefa</button>
       </div>
     </form>
   )
--- src/components/panel/panel_navegation/start/todolist/todolist.js
+++ src/components/panel/panel_navegation/start/todolist/todolist.js
@@ -12,6 +12,8 @@ const TodoList = () => {
         <h5><span><GrCheckbox /></span><p>Todolist</p></h5>
         <h5><span><GrCheckbox /></span><p>Levar comida para o cachorro</p></h5>
         <h5><span><GrCheckbox /></span><p>Todolist</p></h5>
+        <h5><span><GrCheckbox /></span><p>Levar comida para o cachorro</p></h5>
+        <h5><span><GrCheckbox /></span><p>Todolist</p></h5>
         <h5>
           <span id='incompleteItem' onClick={() => changeCheckBox('incompleteItem', 'completeItem')}>
             <GrCheckbox />
